a8b63081b9722d7e080df4e8f3f20cc9dd27fc78
Isaac Tetteh-Apotey <itasofts@gmail.com>
2025-11-27 16:40:01 +0000

    Clean up for Railway deployment - remove pure Python fallbacks, use accurate pyproj version

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,10 +1,4 @@
-try:
-    # Try to import the pyproj version first
-    from .ghana_converter import GhanaWebConverter
-except ImportError:
-    # Fall back to pure Python version
-    from .pure_python_converter import GhanaPurePythonConverter as GhanaWebConverter
-
+from .ghana_converter import GhanaWebConverter
 from .coordinate_utils import dms_to_decimal, decimal_to_dms
 
 __all__ = ['GhanaWebConverter', 'dms_to_decimal', 'decimal_to_dms']
